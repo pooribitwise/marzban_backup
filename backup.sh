d489710a50e3d9c52062e202c153ec28861e07bb
Pourya <adeli.pouria@gmail.com>
2026-06-08 01:04:49 +0330

    Database, .env, xray config backup first commit

---
+++ backup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# vars
+SERVER="USER@HOST"
+BACKUP_DIR="BACKUP_DIRECTORY"
+SSH_KEY="PATH_TO_SSH_KEY"
+SSH_PORT="PORT_FOR_SSH"
+
+mkdir -p "$BACKUP_DIR"
+
+echo "Backup from $SERVER started..."
+
+rsync -avR -e "ssh -i $SSH_KEY -p $SSH_PORT" \
+    $SERVER:/opt/marzban/db.sqlite3 \
+    $SERVER:/opt/marzban/.env \
+    $SERVER:/var/local/etc/xray/config.json \
+    "$BACKUP_DIR"
+
+echo "Backup completed successfully at $BACKUPDIR"
