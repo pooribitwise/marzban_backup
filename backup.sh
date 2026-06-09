eebb24e4edcae67ff1717d2a3afc0e529dc9dd70
Pourya <adeli.pouria@gmail.com>
2026-06-10 02:50:42 +0330

    added nginx reverse proxy config for backup

--- backup.sh
+++ backup.sh
@@ -3,8 +3,8 @@
 # load config
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
-if [ -f "$SCRIPT_DIR/config.env" ]; then
-    source "$SCRIPT_DIR/config.env"
+if [ -f "$SCRIPT_DIR/.env" ]; then
+    source "$SCRIPT_DIR/.env"
     echo "config.env has been loaded successfully"
 else
     echo "Error: config.env does not exist!"
@@ -21,6 +21,7 @@ rsync -avR -e "ssh -i $SSH_KEY -p $SSH_PORT" \
     $SERVER:/opt/marzban/db.sqlite3 \
     $SERVER:/opt/marzban/.env \
     $SERVER:/usr/local/etc/xray/config.json \
+    $SERVER:/etc/nginx/sites-available/default \
     "$BACKUP_DIR"
 
 echo "Backup completed successfully at $BACKUP_DIR"
